df429354e7e8849e4772f2e3b5c6f1fdf7e93d7d
medhansh7190 <73400573+medhansh7190@users.noreply.github.com>
2020-10-26 22:38:06 +0530

    Add files via upload

---
+++ trex/sketch.js
@@ -0,0 +1,32 @@
+var trex,trex_running,trex_collided;
+var ground,invisibleGround,groundImage;
+function preload(){
+  trex_running=loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided=loadImage("trex_collided.png");
+  groundImage=loadImage("ground2.png");
+}
+function setup() {
+  createCanvas(400, 400);
+  trex=createSprite(50,380,20,50);
+  trex.addAnimation("running",trex_running);
+  trex.scale=0.5;
+  ground=createSprite(200,380,400,200);
+  ground.addImage("ground",groundImage);
+  ground.velocityX=-10;
+  invisibleGround=createSprite(200,390,400,10);
+  invisibleGround.visible=false;
+}
+
+function draw() {
+  background(220);
+  if(keyDown("SPACE")){
+ trex.velocityY=-10;
+  }
+  trex.velocityY=trex.velocityY+0.8;
+  if(ground.x<0){
+ ground.x=ground.width/2;
+  }
+  
+  trex.collide(invisibleGround);
+  drawSprites();
+}
